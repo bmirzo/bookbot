81697ff13e2ad0cf67aa7d99866605057c05c18a
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2026-02-27 15:19:41 +0300

    refactored data analytics funcs

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    book = get_book_text("./books/frankenstein.txt")
+    num_words = get_num_words(book)
+    print(f"Found {num_words} total words")
+
+main()
