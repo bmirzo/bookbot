f42a2ee9514a0154d29546345151926b4fb9d745
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2026-02-27 15:45:12 +0300

    char count func

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, char_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,6 +9,8 @@ def get_book_text(path_to_file):
 def main():
     book = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(book)
+    num_chars = char_count(book)
     print(f"Found {num_words} total words")
+    print(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,17 @@
 def get_num_words(book):
     return len(book.split())
+
+def char_count(book):
+    char_dict = {}
+    char_list = []
+
+    words = book.split()
+    for word in words:
+        for char in word:
+            char_list.append(char.lower())
+
+    unique_chars = set("".join(char_list))
+    for ch in unique_chars:
+        char_dict[ch] = char_list.count(ch)
+
+    return char_dict
